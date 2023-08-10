5ffe17fc6e69430fd8ddea4221a59aeeda6f085e
Olstertecn11 <otzunund@miumg.edu.gt>
2023-08-10 13:22:54 -0600

    trying create single class

--- js/graphics.js
+++ js/graphics.js
@@ -60,6 +60,42 @@ var layout = {
 	paper_bgcolor: "rgba(0, 0, 0, 0)"
 };
 
-Plotly.newPlot('myDiv', data, layout, { staticPlot: true });
+
+function createPlot(_component = 'myDiv', _data = data, _layout = layout, _options = { staticPlot: true }) {
+	Plotly.newPlot(_component, _data, _layout, _options);
+}
+
+function downloadPDF(url) {
+	var doc = new jsPDF();
+	var img = new Image();
+	img.src = url;
+	doc.text("PDF de Espectros", 20, 10);
+	doc.addImage(img, 'png', 10, 30, 100, 80);
+	doc.save('Espectros.pdf');
+}
+
+function downloadImage(url) {
+	fetch(url, {
+		mode: 'no-cors',
+	})
+		.then(response => response.blob())
+		.then(blob => {
+			let blobUrl = window.URL.createObjectURL(blob);
+			let a = document.createElement('a');
+			a.download = "grafica_espectros.png";
+			a.href = blobUrl;
+			document.body.appendChild(a);
+			a.click();
+			a.remove();
+		})
+}
+
+const GraphicJS = {
+	create: createPlot,
+	exportPDF: downloadPDF,
+	exportPNG: downloadImage
+}
+
+
 
 
--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+var graphics = GraphicJS;
+
+document.onload = graphics.create()
+
 document.getElementById("btn_img").addEventListener("click", () => {
 	var divGraph = document.getElementById("myDiv");
 	Plotly.toImage('myDiv', { format: 'png', width: 800, height: 600 }).then((dataURL) => {
-		downloadImage(dataURL);
+		graphics.exportPNG(dataURL);
 	})
 });
 
 document.getElementById("btn_pdf").addEventListener("click", () => {
 	var divGraph = document.getElementById("myDiv");
 	Plotly.toImage('myDiv', { format: 'png', width: 800, height: 600 }).then((dataURL) => {
-		addToPDF(dataURL);
+		graphics.exportPDF(dataURL);
 	})
 });
 
